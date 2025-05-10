01b7c705109c8040d0090cb683401392313d50a1
1dcarlson <daniel@altchain.app>
2025-05-10 12:58:22 -0700

    Update email.ts

--- server/email.ts
+++ server/email.ts
@@ -1,6 +1,6 @@
 import sgMail from '@sendgrid/mail';
 
-sgMail.setApiKey(process.env.process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function sendEmail({ to, subject, text, html }) {
   const msg = {
