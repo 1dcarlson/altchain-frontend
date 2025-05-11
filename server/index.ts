9b192bdd3d892d8278e9d17f50f0f0107139172b
1dcarlson <daniel@altchain.app>
2025-05-10 18:28:36 -0700

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -75,11 +75,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 3000;
-server.listen({
-  port,
-  host: "localhost",
-}, () => {
+ const port = 3000;
+server.listen(port, () => {
   log(`✅ Server running at http://localhost:${port}`);
 });
 })();
